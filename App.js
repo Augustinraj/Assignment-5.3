b97845ce87d60ed10d599c01780fb0fcb03ee9b9
Augustin <Augustinraj@users.noreply.github.com>
2016-09-22 14:22:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+
+var employee = {
+
+    "name": "Augus",
+    "Age": 26,
+    "salary": 20000,
+    "address": { "City": "Chennai", "State": "Tamilnadu", "pincode": 600056 }
+};
+// to Display our name, Age, Date of Birth, place of Birth//
+console.log(employee);
